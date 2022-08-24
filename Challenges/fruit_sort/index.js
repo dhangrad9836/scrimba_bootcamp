0ce32b1aca0087fa2ebf0827044f41080afa676c
Darren Dhanpat <darren.dhanpat@yahoo.com>
2022-08-24 13:14:16 -0400

    adding frui sorter

---
+++ Challenges/fruit_sort/index.js
@@ -0,0 +1,26 @@
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"];
+let appleShelf = document.getElementById("apple-shelf");
+let orangeShelf = document.getElementById("orange-shelf");
+
+// Create a function that puts the apples onto the appleShelf
+// and the oranges onto the orangeShelf. Use a for loop,
+// a conditional statement, and the textContent property.
+
+function fruitSort() {
+  for (i = 0; i < fruit.length; i++) {
+    if (fruit[i] === "🍎") {
+      appleShelf.textContent += "🍎";
+    } else if (fruit[i] === "🍊") orangeShelf.textContent += "🍊";
+  }
+}
+
+// function oranges() {
+//   for (i = 0; i < fruit.length; i++) {
+//     if (fruit[i] === "🍊") {
+//       orangeShelf.textContent += "🍊";
+//     }
+//   }
+// }
+
+fruitSort();
+//oranges();
