e10816d3c292cf2762c422e13b2811226ea73cf7
Darren Dhanpat <darren.dhanpat@yahoo.com>
2022-08-25 23:47:59 -0400

    render li items using innerHTML

--- build_chrome_extension/index.js
+++ build_chrome_extension/index.js
@@ -8,6 +8,9 @@ const inputEl = document.getElementById("input-el");
 //first grab input button id
 const inputBtn = document.getElementById("input-btn");
 
+// 2. Grab the unordered list and store it in a const variable called ulEl
+const ulEl = document.getElementById("ul-el");
+console.log(ulEl);
 //second create event listener with click event and function
 //make sure to get value from inputEl and push it to the myLeads array
 inputBtn.addEventListener("click", function () {
@@ -15,6 +18,9 @@ inputBtn.addEventListener("click", function () {
   console.log(myLeads);
 });
 
+//use .innerHTML to use "<li>" to render li items in the html
 for (i = 0; i < myLeads.length; i++) {
-  console.log(myLeads[i]);
+  //console.log(myLeads[i]);
+  //ulEl.textContent += myLeads[i] + ", ";
+  ulEl.innerHTML += "<li>" + myLeads[i] + ", " + "</li>";
 }
